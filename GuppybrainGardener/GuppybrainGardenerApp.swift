d9208e979a6c5098a3a8a186f23985d531d3b4db
Armaan Gupta <armaan@guppy.im>
2025-05-14 21:29:18 -0500

    Initial Commit

---
+++ GuppybrainGardener/GuppybrainGardenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuppybrainGardenerApp.swift
+//  GuppybrainGardener
+//
+//  Created by Armaan Gupta on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GuppybrainGardenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuppybrainGardenerTests/GuppybrainGardenerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuppybrainGardenerTests.swift
+//  GuppybrainGardenerTests
+//
+//  Created by Armaan Gupta on 5/14/25.
+//
+
+import Testing
+@testable import GuppybrainGardener
+
+struct GuppybrainGardenerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
